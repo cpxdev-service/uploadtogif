8249a348eb19a59e06e2f9bb6776ace46155aa0e
chinnathorn.pro <chinnathorn.pro@cubeofnine.asia>
2025-04-17 14:37:35 +0700

    Update

--- GIFConverterOnline/Controllers/UploadController.cs
+++ GIFConverterOnline/Controllers/UploadController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Formats.Gif;
 using SixLabors.ImageSharp.PixelFormats;
+using System;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace GIFConverterOnline.Controllers
@@ -31,38 +32,45 @@ namespace GIFConverterOnline.Controllers
         [ValidateAntiForgeryToken] // Good practice for security against CSRF attacks
         public async Task<IActionResult> UploadImages() // Parameter name matches input name="imageFiles"
         {
-            var files = Request.Form.Files; 
-            if (files == null || files.Count == 0)
+            try
             {
-                ViewBag.Error = "No files were selected.";
-                return View(); // Return to the view with an error message
-            }
+                var files = Request.Form.Files;
+                if (files == null || files.Count == 0)
+                {
+                    ViewBag.Error = "No files were selected.";
+                    return View(); // Return to the view with an error message
+                }
 
-            using var collection = new MagickImageCollection();
-            foreach (var file in files)
+                using var collection = new MagickImageCollection();
+                foreach (var file in files)
+                {
+                    await using var stream = file.OpenReadStream();
+                    var img = new MagickImage(stream);
+                    img.Extent(img.Width, img.Height, Gravity.Forget, MagickColors.Transparent);
+                    collection.Add(img);
+                }
+
+                // Frame configuration
+                foreach (var frame in collection)
+                {
+                    frame.AnimationDelay = 100;
+                    frame.GifDisposeMethod = GifDisposeMethod.Previous;
+                }
+
+                collection.Quantize(new QuantizeSettings { Colors = 256 });
+
+                string guid = Guid.NewGuid().ToString();
+                var output = Path.Combine(
+                    Directory.GetCurrentDirectory(), "blob", $"{guid}.gif");
+                collection.Write(output, MagickFormat.Gif);
+
+                return new JsonResult(new { uploadId = "/download/file/" + guid });
+            }
+            catch (Exception ex)
             {
-                await using var stream = file.OpenReadStream();
-                var img = new MagickImage(stream);
-                img.Extent(img.Width, img.Height, Gravity.Forget, MagickColors.Transparent);
-                collection.Add(img);
+                ViewBag.Error = "An error occurred while processing the images: " + ex.Message;
+                return new JsonResult(new { msg = ex.Message});
             }
-
-            // Frame configuration
-            foreach (var frame in collection)
-            {
-                frame.AnimationDelay = 100;
-                frame.GifDisposeMethod = GifDisposeMethod.Previous;
-            }
-
-            collection.Quantize(new QuantizeSettings { Colors = 256 });
-
-            string guid = Guid.NewGuid().ToString();
-            var output = Path.Combine(
-                Directory.GetCurrentDirectory(), "blob", $"{guid}.gif");
-            collection.Write(output, MagickFormat.Gif);
-
-            return new JsonResult(new { uploadId = "/download/file/" + guid });
         }
-
     }
 }
