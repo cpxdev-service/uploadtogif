43f987560e0c9d8e2ee0d6512e5d64ef2b501e04
chinnathorn.pro <chinnathorn.pro@cubeofnine.asia>
2025-04-17 12:32:29 +0700

    Upload image

--- GIFConverterOnline/Controllers/UploadController.cs
+++ GIFConverterOnline/Controllers/UploadController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ImageMagick;
+using Microsoft.AspNetCore.Mvc;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Formats.Gif;
+using SixLabors.ImageSharp.PixelFormats;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace GIFConverterOnline.Controllers
 {
@@ -13,15 +18,39 @@ namespace GIFConverterOnline.Controllers
         [HttpPost]
         [RequestSizeLimit(100_000_000)]
         [ValidateAntiForgeryToken] // Good practice for security against CSRF attacks
-        public async Task<IActionResult> UploadImages(List<IFormFile> imageFiles) // Parameter name matches input name="imageFiles"
+        public async Task<IActionResult> UploadImages() // Parameter name matches input name="imageFiles"
         {
-            if (imageFiles == null || imageFiles.Count == 0)
+            var files = Request.Form.Files; 
+            if (files == null || files.Count == 0)
             {
                 ViewBag.Error = "No files were selected.";
                 return View(); // Return to the view with an error message
             }
 
-            return View();
+            using var collection = new MagickImageCollection();
+            foreach (var file in files)
+            {
+                await using var stream = file.OpenReadStream();
+                var img = new MagickImage(stream);
+                img.Extent(600, 600, Gravity.Center, MagickColors.Transparent);
+                collection.Add(img);
+            }
+
+            // Frame configuration
+            foreach (var frame in collection)
+            {
+                frame.AnimationDelay = 1000;
+                frame.GifDisposeMethod = GifDisposeMethod.Previous;
+            }
+
+            collection.Quantize(new QuantizeSettings { Colors = 256 });
+
+            string guid = Guid.NewGuid().ToString();
+            var output = Path.Combine(
+                Directory.GetCurrentDirectory(), "blob", $"{guid}.gif");
+            collection.Write(output, MagickFormat.Gif);
+
+            return new JsonResult(new { uploadId = "/download/file/" + guid });
         }
 
     }
---
+++ GIFConverterOnline/Controllers/downloadController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GIFConverterOnline.Controllers
+{
+    public class downloadController : Controller
+    {
+        [HttpGet]
+        public IActionResult file([FromRoute] string id)
+        {
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(), "blob", $"{id}.gif");
+            if (!System.IO.File.Exists(filePath))
+            {
+                return NotFound("File not found.");
+            }
+            byte[] data = System.IO.File.ReadAllBytes(filePath);
+         
+            System.IO.File.Delete(filePath);
+            var contentType = "image/gif";
+            return File(data, contentType, $"{id}.gif");
+        }
+    }
+}
